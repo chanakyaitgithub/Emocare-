1ab72c3de3633d176b8d18ece7d64693c3ed76c9
chanakyaitgithub <149157771+chanakyaitgithub@users.noreply.github.com>
2024-08-12 22:34:46 +0530

    Update groupchatpage.dart

--- groupchatpage.dart
+++ groupchatpage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'message.dart';
 
 class ChatPage extends StatefulWidget {
   final String email;
@@ -13,8 +13,9 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  int count = 0;
+  final TextEditingController _messageController = TextEditingController();
   bool _showImage = false;
+  int count = 0;
   String highestEmotion = '';
   Map<String, List<double>> _emotionScores = {
     'joy': [],
@@ -26,9 +27,6 @@ class _ChatPageState extends State<ChatPage> {
     'surprise': [],
     'neutral': [],
   };
-
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final TextEditingController _messageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +40,7 @@ class _ChatPageState extends State<ChatPage> {
           children: [
             DrawerHeader(
               decoration: BoxDecoration(
-                color: Color.fromARGB(255, 16, 233, 139),
+                color: Colors.deepOrange,
               ),
               child: Text('Sentiment Analysis Results'),
             ),
@@ -59,14 +57,14 @@ class _ChatPageState extends State<ChatPage> {
         children: [
           Positioned.fill(
             child: Image.asset(
-              'fan/m3.jpeg', // Replace with your image asset path
+              'fan/bg5.png', // Replace with your image asset path
               fit: BoxFit.cover,
             ),
           ),
           Column(
             children: [
               Expanded(
-                child: _buildMessageList(), // Use StreamBuilder for messages
+                child: Message(email: widget.email), // Use Message widget
               ),
               _buildMessageInput(),
               ElevatedButton(
@@ -92,12 +90,15 @@ class _ChatPageState extends State<ChatPage> {
               controller: _messageController,
               style: TextStyle(color: Colors.white),
               decoration: InputDecoration(
-                  hintText: 'Type your message...',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey, // Change the color here
-                    ),
-                  )),
+                hintText: 'Type your message...',
+                hintStyle: TextStyle(
+                    color: Colors.white), // Change hint text color to white
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
             ),
           ),
           IconButton(
@@ -114,85 +115,57 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  Widget _buildMessageList() {
-    return StreamBuilder<QuerySnapshot>(
-      stream:
-          _firestore.collection('messages').orderBy('timestamp').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        final messages = snapshot.data!.docs.reversed;
-        List<MessageBubble> messageBubbles = [];
-        for (var message in messages) {
-          final messageText = message['text'];
-          final messageSender = message['sender'];
-
-          final messageBubble = MessageBubble(
-            sender: messageSender,
-            text: messageText,
-            isMe: widget.email == messageSender,
-            showImage: _showImage,
-          );
-          messageBubbles.add(messageBubble);
-        }
-
-        return Expanded(
-          child: ListView(
-            reverse: true,
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-            children: messageBubbles,
-          ),
-        );
-      },
-    );
-  }
-
   void _sendMessage() async {
     String messageText = _messageController.text.trim();
     if (messageText.isNotEmpty) {
-      _firestore.collection('messages').add({
-        'text': messageText,
-        'sender': widget.email,
-        'timestamp': DateTime.now(),
-      });
-      _messageController.clear();
+      final url = Uri.parse('http://localhost:3000/messages');
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'sender': widget.email,
+          'message': messageText,
+        }),
+      );
 
-      // Perform sentiment analysis if message text is not null
-      try {
-        if (messageText != null) {
+      if (response.statusCode == 201) {
+        _messageController.clear();
+        try {
           Map<String, dynamic> sentimentResult =
               await performSentimentAnalysis(messageText);
-          // Add sentiment analysis result to emotion scores
           sentimentResult.forEach((key, value) {
             if (_emotionScores.containsKey(key)) {
-              _emotionScores[key]?.add(value);
+              double parsedValue = double.tryParse(value.toString()) ?? 0.0;
+              _emotionScores[key]?.add(parsedValue);
             }
           });
+
           setState(() {
-            // Track highest emotion
-            if (sentimentResult['label'] != null) {
-              highestEmotion = sentimentResult['label'];
-            }
-            // Check if the highest emotion is sad and count exceeds threshold
+            double highestValue = 0.0;
+            highestEmotion = '';
+            sentimentResult.forEach((key, value) {
+              double parsedValue = double.tryParse(value.toString()) ?? 0.0;
+              if (parsedValue > highestValue) {
+                highestValue = parsedValue;
+                highestEmotion = key;
+              }
+            });
+
             if (highestEmotion == 'sad' && count >= 4) {
               _showImage = true;
               count = 0;
             } else {
               _showImage = false;
-              // Increment count if emotion is sad
               if (highestEmotion == 'sad') {
                 count++;
               }
             }
-          }); // Refresh UI
+          });
+        } catch (e) {
+          print('Error performing sentiment analysis: $e');
         }
-      } catch (e) {
-        print('Error performing sentiment analysis: $e');
-        // Handle error here
+      } else {
+        print('Failed to send message');
       }
     }
   }
@@ -207,15 +180,13 @@ class _ChatPageState extends State<ChatPage> {
     );
 
     if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      return data;
+      return json.decode(response.body);
     } else {
       throw Exception('Failed to fetch sentiment analysis result');
     }
   }
 
   void _updateEmotionScores() {
-    // Calculate the average score for each emotion class
     final Map<String, double> averageEmotionScores = {};
 
     _emotionScores.forEach((key, value) {
@@ -224,7 +195,6 @@ class _ChatPageState extends State<ChatPage> {
       averageEmotionScores[key] = totalCount != 0 ? totalScore / totalCount : 0;
     });
 
-    // Display the average scores
     showDialog(
       context: context,
       builder: (context) {
@@ -239,79 +209,6 @@ class _ChatPageState extends State<ChatPage> {
           ),
         );
       },
-    );
-  }
-}
-
-class MessageBubble extends StatelessWidget {
-  final String sender;
-  final String text;
-  final bool isMe;
-  final bool showImage;
-
-  MessageBubble(
-      {required this.sender,
-      required this.text,
-      required this.isMe,
-      required this.showImage});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment:
-            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-        children: [
-          Text(
-            sender,
-            style: TextStyle(
-              fontSize: 12.0,
-              color: Colors.white54,
-            ),
-          ),
-          Material(
-            borderRadius: isMe
-                ? BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    bottomLeft: Radius.circular(30.0),
-                    bottomRight: Radius.circular(30.0),
-                  )
-                : BorderRadius.only(
-                    topRight: Radius.circular(30.0),
-                    bottomRight: Radius.circular(30.0),
-                    bottomLeft: Radius.circular(30.0),
-                  ),
-            elevation: 5.0,
-            color: isMe ? Color.fromARGB(255, 16, 233, 139) : Colors.white,
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '$text',
-                    style: TextStyle(
-                      color: isMe ? Colors.white : Colors.black54,
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  if (!isMe && showImage) // Show image conditionally
-                    SizedBox(
-                      height: 50,
-                      width: 50,
-                      child: Image.asset(
-                        'fan/imagg.jpg',
-                        width: 200, // Adjust width as needed
-                        height: 200, // Adjust height as needed
-                      ),
-                    ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
